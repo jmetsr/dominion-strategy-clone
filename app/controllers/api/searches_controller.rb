b4b67dc21f0be0b532ce92be3a6379cce1f6ac7d
Jeremy Jaffe <jmetsr@gmail.com>
2014-12-02 13:00:22 -0500

    undo thing I did for debugging

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -2,10 +2,8 @@ class Api::SearchesController < ApplicationController
 
   def index
 
-    #@results = PgSearch.multisearch(params[:query]).includes(:searchable).map(&:searchable)
-    @results = PgSearch.multisearch("outage").includes(:searchable).map(&:searchable)
+    @results = PgSearch.multisearch(params[:query]).includes(:searchable).map(&:searchable)
     render :show
-    #render :json => @results
   end
 
 end
